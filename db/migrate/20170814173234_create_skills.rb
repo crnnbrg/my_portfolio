5a2ff159beae19b71802b60a48aa5f6824b32296
Betty Mutai <bettymutai00@gmail.com>
2017-08-14 21:49:30 +0300

    Created table skills with its actions and model and defined the route

---
+++ db/migrate/20170814173234_create_skills.rb
@@ -0,0 +1,10 @@
+class CreateSkills < ActiveRecord::Migration[5.1]
+  def change
+    create_table :skills do |t|
+      t.colimn :name, :string
+      t.column :description, :string
+
+      t.timestamps
+    end
+  end
+end
