12a8b975e376dab0eee477b886e537ed238c7e8c
Betty Mutai <bettymutai00@gmail.com>
2017-08-14 22:09:54 +0300

    Added carrierwave gem to upload projects pictures

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -69,6 +69,6 @@ class SkillsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def skill_params
-      params.fetch(:skill, {})
+      params.require(:skill).permit(:name, :description)
     end
 end
