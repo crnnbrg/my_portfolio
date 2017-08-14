737522b776cbb42622979a21f8c7ced9138fdad3
Betty Mutai <bettymutai00@gmail.com>
2017-08-14 22:12:45 +0300

    Nested projects under skills

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :projects
-  resources :skills
+  resources :skills do
+    resources :projects
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
