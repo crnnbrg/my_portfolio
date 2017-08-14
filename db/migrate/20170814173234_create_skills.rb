a5e0d88be5834c7019e45e82fd89e82a68c415c0
Betty Mutai <bettymutai00@gmail.com>
2017-08-14 21:58:49 +0300

    Created table projects with its actions and model and defined the route

--- db/migrate/20170814173234_create_skills.rb
+++ db/migrate/20170814173234_create_skills.rb
@@ -1,7 +1,7 @@
 class CreateSkills < ActiveRecord::Migration[5.1]
   def change
     create_table :skills do |t|
-      t.colimn :name, :string
+      t.column :name, :string
       t.column :description, :string
 
       t.timestamps
--- db/migrate/20170814185156_create_projects.rb
+++ db/migrate/20170814185156_create_projects.rb
@@ -1,6 +1,11 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
+      t.column :name, :string
+      t.column :picture, :string
+      t.column :link, :string
+      t.column :description, :string
+      t.column :skill_id, :integer
 
       t.timestamps
     end
