2f95935c7372c120a649f660bf4d15748ce8a987
Betty Mutai <bettymutai00@gmail.com>
2017-08-16 17:28:08 +0300

    Changed blog to posts and added a column user_id to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :posts
   devise_for :users
   root :to => 'main#index'
   resources :skills do
--- db/migrate/20170816115124_add_column_user_id.rb
+++ db/migrate/20170816115124_add_column_user_id.rb
@@ -1,5 +1,5 @@
 class AddColumnUserId < ActiveRecord::Migration[5.1]
   def change
-    add_column :blogs, :user_id, :integer
+    add_column :posts, :user_id, :integer
   end
 end
