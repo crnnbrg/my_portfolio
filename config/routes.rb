82a2b74bab4c6bee312a36e1076e7dbf04c77a04
Betty Mutai <bettymutai00@gmail.com>
2017-08-17 09:15:14 +0300

    Removed extra resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  resources :comments
   devise_for :users
   root :to => 'main#index'
   resources :skills do
     resources :projects
   end
-  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
